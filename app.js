fd6687c8d9d7127ea747186b4c01def2ee68ce3d
Azizullah <azizullahcodes@gmail.com>
2025-12-23 22:21:21 +0500

    incluedes method

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ let heading = document.getElementById('heading') ;
 let btn = document.getElementById('btn');
 let img = document.getElementById('bulb') ;
 let isOn = false;
-function bulbOnOff(){if(isOn){isOn = false;
+function bulbOnOff(){if(img.src.includes('off_bulb.jfif')){
     heading.innerHTML = 'Bulb On';
     btn.innerHTML = 'Bulb Off';
     img.src = "./assets/on-bulb.jfif";
 }
-else{isOn = true;
+else{
     heading.innerHTML = 'Bulb off';
     btn.innerHTML = 'Bulb on';
     img.src = './assets/off_bulb.jfif';
